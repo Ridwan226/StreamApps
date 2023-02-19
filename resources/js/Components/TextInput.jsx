f105551a9f364b02ce5b936876ba1d5010668740
Ridwan Ganteng <ridwan.endan26@gmail.com>
2023-02-19 19:20:33 +0700

    Sign In Sign Up

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,4 +1,20 @@
 import { forwardRef, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
+forwardRef.propTypes = {
+    type: PropTypes.oneOf(["text", "email", "password", "number", "file"]),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    varian: PropTypes.oneOf(["primary", "error", "primary-outline"]),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool,
+};
 
 export default forwardRef(function TextInput(
     {
@@ -33,9 +49,11 @@ export default forwardRef(function TextInput(
                 name={name}
                 id={id}
                 value={value}
+                defaultValue={defaultValue}
                 className={
-                    `rounded-2xl bg-form-bg py-[13px] px-7 w-full input-${varian} ` +
-                    className
+                    `rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                        isError && "input-error"
+                    } input-${varian} ` + className
                 }
                 ref={input}
                 autoComplete={autoComplete}
--- resources/js/Pages/Prototype/Register.jsx
+++ resources/js/Pages/Prototype/Register.jsx
@@ -1,12 +1,12 @@
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
-import { Head, Link } from "@inertiajs/react";
+import { Link, Head } from "@inertiajs/react";
 
-export default function Login() {
+export default function Register() {
     return (
         <>
-            <Head title="SignIn" />
+            <Head title="SignUp" />
             <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
                 <div className="fixed top-[-50px] hidden lg:block">
                     <img
@@ -20,7 +20,7 @@ export default function Login() {
                         <img src="/assets/images/moonton-white.svg" alt="" />
                         <div className="my-[70px]">
                             <div className="font-semibold text-[26px] mb-3">
-                                Welcome Back
+                                Sign Up
                             </div>
                             <p className="text-base text-[#767676] leading-7">
                                 Explore our new movies and get <br />
@@ -31,61 +31,50 @@ export default function Login() {
                             <div className="flex flex-col gap-6">
                                 <div>
                                     <InputLabel
-                                        forInput="email"
-                                        value={"Email Address"}
+                                        value={" Full Name"}
+                                        forInput="fullname"
                                     />
-                                    {/* <input
-                                    type="email"
-                                    name="email"
-                                    className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
-                                    placeholder="Email Address"
-                                /> */}
                                     <TextInput
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email Address"
-                                        varian="primary"
+                                        type="text"
+                                        name="fullname"
+                                        placeholder="Your fullname..."
                                     />
                                 </div>
                                 <div>
                                     <InputLabel
-                                        forInput="password"
+                                        value={"Email Address"}
+                                        forInput="email"
+                                    />
+                                    <TextInput
+                                        type="email"
+                                        name="email"
+                                        placeholder="Your Email Address"
+                                    />
+                                </div>
+                                <div>
+                                    <InputLabel
                                         value={"Password"}
+                                        forInput="password"
                                     />
                                     <TextInput
                                         type="password"
                                         name="password"
-                                        varian="primary"
-                                        placeholder="Password"
+                                        placeholder="Your Email Address"
                                     />
                                 </div>
                             </div>
                             <div className="grid space-y-[14px] mt-[30px]">
-                                {/* <a
-                                href="/"
-                                className="rounded-2xl bg-alerange py-[13px] text-center"
-                            >
-                                <span className="text-base font-semibold">
-                                    Start Watching
-                                </span>
-                            </a> */}
                                 <Link>
-                                    <PrimaryButton
-                                        type="button"
-                                        varian="primary"
-                                    >
+                                    <PrimaryButton varian="primary">
                                         <span className="text-base font-semibold">
-                                            Start Watching
+                                            Sign Up
                                         </span>
                                     </PrimaryButton>
                                 </Link>
-                                <Link href={route("prototype.register")}>
-                                    <PrimaryButton
-                                        type="button"
-                                        varian="light-outline"
-                                    >
+                                <Link href={route("prototype.login")}>
+                                    <PrimaryButton varian="light-outline">
                                         <span className="text-base text-white">
-                                            Create New Account
+                                            Sign In to My Account
                                         </span>
                                     </PrimaryButton>
                                 </Link>
